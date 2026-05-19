b0f23afa162246edde121ad2fde4a971f2f453e1
Aleksandr Kudriashov <alexander.a.kudryashov@yandex.ru>
2026-05-19 03:13:33 +0300

    add test

---
+++ src/test/java/com/tcpproxy/configuration/ConfigurationParserTest.java
@@ -0,0 +1,20 @@
+package com.tcpproxy.configuration;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigurationParserTest {
+
+    @Test
+    void parseConfigurationReturnsValidEntries() throws IOException {
+        List<ConfigurationEntry> entries = ConfigurationParser.parseConfiguration("/proxy.properties");
+        assertNotNull(entries);
+        assertEquals(1, entries.size());
+        ConfigurationEntry entry = entries.get(0);
+        assertEquals(8080, entry.getLocalPort());
+        assertEquals("localhost", entry.getRemoteHost());
+        assertEquals(9090, entry.getRemotePort());
+    }
+}
